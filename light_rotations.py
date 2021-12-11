3f8810860bd69eb21548e53c0f75c6fb9cd4ad0e
cadulceus <a@DESKTOP-QVE5S07.localdomain>
2021-12-10 21:31:14 -0800

    clean up light_rotations

--- light_rotations.py
+++ light_rotations.py
@@ -1,4 +1,4 @@
-import neopixel, board, time, code
+import neopixel, board, time
 
 PIXELS_COUNT = 250
 INIT_COLOR_SCALE = 5
@@ -6,9 +6,6 @@ STEP_COLOR_SCALE = 15
 
 pixels = neopixel.NeoPixel(board.D21, PIXELS_COUNT)
 pixels.auto_write = False
-# pixels.fill((255, 255, 0))
-# pixels.brightness = 0.1
-# exit(0)
 
 # assumes that at least one color is 0 and r + g + b <= 255
 def rotate_color(grb, count=1, scale=STEP_COLOR_SCALE):
@@ -21,13 +18,12 @@ def rotate_color(grb, count=1, scale=STEP_COLOR_SCALE):
 
             rotated_grb[i] = max(0, rotated_grb[i] - scale)
             rotated_grb[(i + 1) % 3] = min(255, rotated_grb[(i + 1) % 3] + scale)
-            #print(rotated_grb)
         return rotated_grb
 
 pixels.fill((255,0,0))
 print("initializing colors")
 for i, p in enumerate(pixels):
-    pixels[i] = rotate_color(p, i, INIT_COLOR_SCALE) # (p[0] - (i * INIT_COLOR_SCALE), (i * INIT_COLOR_SCALE), 0)
+    pixels[i] = rotate_color(p, i, INIT_COLOR_SCALE)
 print(pixels)
 pixels.show()
 
@@ -39,7 +35,3 @@ while 1:
     print(pixels, "\n\n")
     time.sleep(0.01)
     pixels.show()
-
-#p.show()
-#code.interact(local = locals())
-#p.deinit()
